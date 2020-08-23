b9fb3839259757b5463be67db6c459a4abf4e6ae
Nick Reyno <nickrenoe@gmail.com>
2020-08-22 21:11:04 -0400

    fixing a mistake

--- wp-content/themes/fictional-university-theme/template-parts/content-project.php
+++
@@ -1,8 +0,0 @@
-<div class="post-item">
-    <li class="project-card__list-item">
-            <a class="project-card" href="<?php the_permalink(); ?>">
-            <img class="project-card__image" src="<?php the_post_thumbnail_url('project_landscape')?>" alt="">
-            <span class="project-card__name"> <?php the_title()?></span>
-        </a>
-    </li>
-</div>
